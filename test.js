ce2ed73923afc90b00daf6f39963b6962df6b630
Insight Zen <85638325+insight-zen@users.noreply.github.com>
2022-02-12 08:22:57 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,8 @@
-b = document.querySelector("body")
-b.insertAdjacentHTML("afterbegin", "<p class='blue ba_1'>Hellp again</p>")
+function handle_dcl(){
+  console.log(" * Begin handle dcl")
+  let b = document.querySelector("body")
+  b.insertAdjacentHTML("afterbegin", "<p class='blue ba_1'>Hellp again</p>")
+
+}
+
+document.addEventListener('DOMContentLoaded', () => { handle_dcl(); });
